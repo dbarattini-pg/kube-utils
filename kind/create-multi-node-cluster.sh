2b89aae752f91a75273ebac83f2a5eb3728a8c26
dbarattini-pg <dbarattini-pg@outlook.it>
2021-09-12 19:11:23 +0200

    add ingress-nginx config file and script options

--- kind/create-multi-node-cluster.sh
+++ kind/create-multi-node-cluster.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-kind.exe create cluster --config $(dirname "$0")/multi-node-cluster-config.yml
+config=$(dirname "$0")/multi-node-cluster-config.yml
+nginx=false
+wsl2=false
+
+while getopts nw flag
+do
+    case "${flag}" in
+        n)  config=$(dirname "$0")/ingress-nginx-multi-node-cluster-config.yml
+            nginx=true
+            ;;
+        w)  wsl2=true
+    esac
+done
+
+kind.exe create cluster --config $config
+
+if [ "$wsl2" = true ] ; then
+    echo -e '\nWriting kind kubeconfig to /tmp/kubeconfig... ✍🏻'
+    kind.exe get kubeconfig > /tmp/kubeconfig
+    export KUBECONFIG=$KUBECONFIG:/tmp/kubeconfig
+fi
+
+if [ "$nginx" = true ] ; then
+    if [ "$wsl2" = false ] ; then
+        echo ''
+    fi
+    echo 'Installing ingress Nginx... 🚪'
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+fi
+
+if [ "$wsl2" = true ] ; then
+    echo -e '\n\033[1;33mAdd kind kubeconfig to env with: export KUBECONFIG=$KUBECONFIG:/tmp/kubeconfig\033[0m\n'
+fi
--- kind/wsl2-kubeconfig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# run with: source wsl2-kubeconfig.sh
-kind.exe get kubeconfig > /tmp/kubeconfig
-export KUBECONFIG=/tmp/kubeconfig
-
